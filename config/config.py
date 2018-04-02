b85f00f1ef53aa4f93afc103a13b51fe61986f88
Alex <ostrich_king@163.com>
2018-04-02 14:57:29 +0800

    bugfix.

--- config/config.py
+++ config/config.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from configparser import ConfigParser
+import six
+if six.PY2:
+    import ConfigParser as configparser
+elif six.PY3:
+    import configparser
 import os
 
 
-cfg = ConfigParser()
-cfg.readfp(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.cfg')))
+cfg = configparser.ConfigParser()
+cfg.read_file(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.cfg')))
 print(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.cfg'))
 
 avatar_path = cfg.get('avatar', 'img_path')
--- news/views.py
+++ news/views.py
@@ -6,13 +6,13 @@ from services.web import login_check
 # Create your views here.
 
 
-@login_check
+# @login_check
 def index(req):
-    return redirect("/news/wx_index")
+    return redirect("/news/wx_index", permanent=True)
     return HttpResponse("Hello, I'm oneline. New style to achieve news.")
 
 
-@login_check
+# @login_check
 def create_item(req):
     if req.method != 'POST':
         raise Exception('Not a post request!')
@@ -20,7 +20,7 @@ def create_item(req):
     return HttpResponse('You have post a oneline: ' + req.POST.get('title'))
 
 
-@login_check
+# @login_check
 def wx_index(req):
     return HttpResponse("We have redirect here.")
 
