aaf119dc6cc32039996a72ea75c9cf10161d1324
Alex <ostrich_king@163.com>
2018-04-02 14:17:09 +0800

    bugfix.

--- config/config.py
+++ config/config.py
@@ -6,6 +6,6 @@ import os
 
 cfg = ConfigParser()
 cfg.readfp(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.cfg')))
-print os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.cfg')
+print(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.cfg'))
 
 avatar_path = cfg.get('avatar', 'img_path')
